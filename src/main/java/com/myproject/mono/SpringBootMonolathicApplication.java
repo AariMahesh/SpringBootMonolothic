88957300dc8104cb50678080292be29b8056956a
AariMahesh <maheshaari4455@gmail.com>
2026-08-10 12:10:44 +0530

    first commit

---
+++ src/main/java/com/myproject/mono/SpringBootMonolathicApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMonolathicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMonolathicApplication.class, args);
+	}
+
+}
